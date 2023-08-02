c4ad0b123a546d60a685f3e99fa5ad776091799d
Veronika Aniskova <v9037@yandex.ru>
2023-08-02 12:28:19 +0300

    14ПР: проверка автотестов fix signup

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,6 @@ const regularExpression = /^(https?:\/\/)(www\.)?[-a-zA-Z0-9._~:/?#[\]@!$&'()*+,
 
 router.post('/signin', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(regularExpression),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   })
@@ -21,6 +18,9 @@ router.post('/signin', celebrate({
 
 router.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(regularExpression),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   })
